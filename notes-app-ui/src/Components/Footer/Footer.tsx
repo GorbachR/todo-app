af2ab1bf89587a615797051602493785fd230b56
GorbachR <raphael_gorbach@gmx.at>
2023-04-30 06:10:39 +0200

    Deletion:
    delete old ui

--- notes-app-ui/src/Components/Footer/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="">
-      <span className="text-slate-600 mx-auto block w-fit p-4 text-center">© Raphael Gorbach</span>
-    </footer>
-  );
-}
--- notes-app-ui/src/hooks/useGetNotes.ts
+++
@@ -1,8 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
-
-export default function useGetNotes() {
-  return useQuery(["products"], () => {
-    return axios.get(import.meta.env.VITE_NOTESAPI).then((res) => res.data);
-  });
-}
