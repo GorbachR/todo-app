509e9f5e8550af1e7719f38dd50fd3c12420fbc0
GorbachR <raphael_gorbach@gmx.at>
2023-09-11 18:42:23 +0200

    Fix: cleanin up main branch

--- notes-app-api/src/main/java/notesappapi/repository/NotesRepository.java
+++
@@ -1,9 +0,0 @@
-package notesappapi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import notesappapi.entity.Notes;
-
-public interface NotesRepository extends JpaRepository<Notes, Long> {
-    
-}
